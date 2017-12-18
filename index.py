c94488f7512f192b6c07b8fc556957e0f5052286
Eshita <e.shrawan@gmail.com>
2017-12-18 18:51:13 +0530

    bubble

--- index.py
+++ index.py
@@ -30,6 +30,22 @@ def dividePage():
     b = request.args.get('B')
     return str((int(a)/float(b)))
 
+@app.route("/bubblesort", methods=["GET","POST"])
+def bubblePage():
+    a= request.args.get('A')
+    b = request.args.get('B')
+    c = request.args.get('C')
+    d = request.args.get('D')
+    e = request.args.get('E')
+    number = [a,b,c,d,e]
+    numberlen = len(number)
+    for k in range(numberlen-1, 0 ,-1):
+        for i in range (k):
+            if(number[i+1]< number[i]):
+                number[i],number[i+1] = number[i+1],number[i]
+    print(number)
+
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port = port, debug = True)
