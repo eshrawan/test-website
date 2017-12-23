139527541a03e429058858ad2bb1ae4fa0e54b56
Eshita <e.shrawan@gmail.com>
2017-12-23 11:11:28 +0530

    t

--- index.py
+++ index.py
@@ -13,14 +13,30 @@ def formPage():
 @app.route("/submit", methods=["POST"])
 def submitPage():
     a = request.form.get('fvalue')
-try:
-    value=int(a)
-except ValueError:
-    print("This is not a whole number.")
+    try:
+        value=int(a)
+    except ValueError:
+        return("The first value is not a number.")
     b = request.form.get('svalue')
+    try:
+        value=int(b)
+    except ValueError:
+        return("The second value is not a number.")
     c = request.form.get('tvalue')
+    try:
+        value=int(c)
+    except ValueError:
+        return("The third value is not a number.")
     d = request.form.get('fovalue')
+    try:
+        value=int(d)
+    except ValueError:
+        return("The fourth value is not a number.")
     e = request.form.get('fivalue')
+    try:
+        value=int(e)
+    except ValueError:
+        return("The fifth value is not a number.")
     number = [int(a),int(b),int(c),int(d),int(e)]
     numberlen = len(number)
     for k in range(numberlen-1, 0 ,-1):
@@ -28,7 +44,7 @@ except ValueError:
             if(number[i+1]< number[i]):
                 number[i],number[i+1] = number[i+1],number[i]
     return str(number)
-return app.send_static_file('result.html')
+    return app.send_static_file('result.html')
 
 @app.route("/add", methods=["GET","POST"])
 def addPage():
