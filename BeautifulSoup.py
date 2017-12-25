66361fa9c98abd9f3ffb807bdf66131343d241fa
Eshita <e.shrawan@gmail.com>
2017-12-25 14:29:48 +0530

    g

--- BeautifulSoup.py
+++ BeautifulSoup.py
@@ -2,12 +2,18 @@ from bs4 import BeautifulSoup
 import requests
 
 
-r=requests.get("http://www.moneycontrol.com/india/stockpricequote/infrastructure-general/larsentoubro/LT")
+r=requests.get("http://go4mumbai.com/Mumbai_Bus_Route.php?busno=0010&bustype=RegularRoutes&busno1=0010&busno2=notselected&busno3=notselected&busno4=notselected")
+
 #print(r.text)
 
 html_doc=r.text
 soup = BeautifulSoup(html_doc, 'html.parser')
-span=soup.find_all("span", { "id" : "Bse_Prc_tick"})
-print(span[0].text)
+span=soup.find("table", {"class" : "train"})
+for row in span.findAll("tr"):
+    cells = row.findAll("a")
+    for cell in cells:
+        print cell.text
+        print("-----")
+    
 
     
